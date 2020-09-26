1d670d3f80910950ef1f39fc0e6ec4629655c626
ristoxxx <lahteri@gmail.com>
2020-09-26 18:24:10 +0300

    Updates

---
+++ precache-manifest.19dd71d4d2eed7d00e93dd94eea3104c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59aec7a56692a942b66a61a6a9aa3cb4",
+    "url": "/ristoxxx/ristoxxx.github.io.git/index.html"
+  },
+  {
+    "revision": "b1f8288e52c0756ee2ae",
+    "url": "/ristoxxx/ristoxxx.github.io.git/static/js/2.1e571c22.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ristoxxx/ristoxxx.github.io.git/static/js/2.1e571c22.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76ec4350a89fb5d0e46a",
+    "url": "/ristoxxx/ristoxxx.github.io.git/static/js/main.4327a8e1.chunk.js"
+  },
+  {
+    "revision": "2e3810e6fe1346399a8c",
+    "url": "/ristoxxx/ristoxxx.github.io.git/static/js/runtime-main.23600049.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/laskin/precache-manifest.d1a09beaf97573ff67ee5e0e7a16ec79.js"
+  "/ristoxxx/ristoxxx.github.io.git/precache-manifest.19dd71d4d2eed7d00e93dd94eea3104c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/laskin/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ristoxxx/ristoxxx.github.io.git/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
